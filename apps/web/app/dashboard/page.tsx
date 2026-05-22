31e3e67d3cc2b91fcd3fd89800422b2553ee95a6
ramesh-kunwar <rameshkunwar1110@gmail.com>
2026-05-22 13:59:47 +0545

    feat: add dashboard page

---
+++ apps/web/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div className=" flex items-center justify-center h-screen">
+        <h1 className="text-8xl font-black ">Dashboard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
